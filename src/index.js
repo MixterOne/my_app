fcf2526968d279e07c377d1abbe3a9517dd011b8
MixterOne <matheusisaias879@gmail.com>
2022-05-02 12:11:57 -0300

    Estilos globais e configuracao de tema

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { purple, red } from '@material-ui/core/colors';
+
 import App from './App';
+import './index.css'
+
+const theme = createMuiTheme({
+  palette: {
+    primary:{
+      main: purple[500],
+    } 
+  },
+  secondary:{
+    main: red[700],
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+
   </React.StrictMode>
 );
 
